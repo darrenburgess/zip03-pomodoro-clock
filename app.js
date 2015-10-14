274a3bf55b3f8ae851e7ee81306ee8b8d11f6711
Darren Burgess <darren_b@beezwax.net>
2015-10-14 09:55:14 -0400

    added basic timer javascript

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 $(document).ready(function(){
-	$('button').click(function(){
-		console.log('jquery is working');
-	})
+
+	function startTimer(duration, display) {
+		var timer = duration, minutes, seconds;
+		setInterval(function(){
+			minutes = parseInt(timer / 60, 10);
+			seconds = parseInt(timer % 60, 10);
+
+			minutes = minutes < 10 ? "0" + minutes : minutes;
+			seconds = seconds < 10 ? "0" + seconds : seconds;
+
+			display.text(minutes + ":" + seconds);
+
+			if (--timer < 0) {
+				timer = duration;
+			}
+		}, 1000);
+	}
+
+	$('#start').click(function () {
+		var fiveMinutes = 60 * 5,
+			display = $('#time');
+		startTimer(fiveMinutes, display);
+	});
+
 });

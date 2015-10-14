7411b22a6110e21c9c9868d6dc78590d0644cdbb
Darren Burgess <darren_b@beezwax.net>
2015-10-14 10:15:38 -0400

    initialized timer values

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 $(document).ready(function(){
 
+	function setDefaultTimes() {
+		workTime = "25:00";
+		breakTime = "5:00";
+		$('#workTime').append(workTime);
+		$('#breakTime').append(breakTime);
+	}
+
+	setDefaultTimes();
+
+	function increment(type, direction){
+		// increments the break timer or pomodoro timer by one minute up or down
+	}
+
 	function startTimer(duration, display) {
 		var timer = duration, minutes, seconds;
 		setInterval(function(){
@@ -17,10 +30,18 @@ $(document).ready(function(){
 		}, 1000);
 	}
 
+	function stopTimer() {
+		console.log('timer stopped');
+	}
+
 	$('#start').click(function () {
 		var fiveMinutes = 60 * 5,
 			display = $('#time');
 		startTimer(fiveMinutes, display);
+	});
+
+	$('#stop').click(function () {
+		stopTimer();
 	});
 
 });
